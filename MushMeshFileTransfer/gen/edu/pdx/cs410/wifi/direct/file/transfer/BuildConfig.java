c90bd7dc7f930c4497490a3a461d7a2efd0fbba0
Jing-Qiu <qiujin@seas.upenn.edu>
2013-10-13 12:41:43 +0800

    delete files(reverted from commit 1ffb6ecc7d1e132281a232e5cdda961415c66d35)

--- MushMeshFileTransfer/gen/edu/pdx/cs410/wifi/direct/file/transfer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.pdx.cs410.wifi.direct.file.transfer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
